0442680b3e085b3f9d32420ae0c2a4c9f2f90c1e
nidhinr07 <nidhinjunior117@gmail.com>
2026-07-27 22:33:33 +0530

    Update task1-calculator.py

--- task1-calculator.py
+++ task1-calculator.py
@@ -1,22 +1,31 @@
 while True:
+    
     print("\n************   CALCULATOR   **************")
     a=int(input("\nEnter First Number : "))
     b=int(input("Enter Second Number : "))
     choice=input("Enter Your Operation [+, -, *, /, //, %, exit] : ").lower().strip()
+    
     if choice=="exit":
         print("\nThanks For Using this Calculator")
         break
+        
     elif choice=="+":
         print("\nAddition Result:",a+b)
+        
     elif choice=="-":
         print("\nSubtraction Result:",a-b)
+        
     elif choice=="*":
         print("\nMultiplication Result:",a*b)
+        
     elif choice=="/":
         print("\nDivision Result:",a/b)
+        
     elif choice=="//":
         print("\nFloor Division Result:",a//b)
+        
     elif choice=="%":
         print("\nModules Result:",a%b)
+        
     else:
         print("\nInvalid Choice,Enter Correct Choice")
